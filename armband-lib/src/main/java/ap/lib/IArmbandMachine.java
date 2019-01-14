de83c7f88f0d369864772c7a3be4d42177f200f0
gggordon <ggg_gilroy@yahoo.com>
2019-01-14 16:36:19 -0500

    Armband Machine Interface

---
+++ armband-lib/src/main/java/ap/lib/IArmbandMachine.java
@@ -0,0 +1,12 @@
+package ap.lib;
+
+public interface IArmbandMachine {
+	/**
+	 * <i>getCost</i>
+	 * Calculates the cost of an armband
+	 * @param price - cost of the armband
+	 * @param quantity - amount of armbands
+	 * */
+	float getCost(float price, int quantity);
+
+}
